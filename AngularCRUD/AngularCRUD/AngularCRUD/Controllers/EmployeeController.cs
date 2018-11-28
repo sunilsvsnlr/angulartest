ed1ae1f5e88376bd9699e641c2fd8f2c949d5ba3
Suneel <sunilsvsnlr@gmail.com>
2018-11-28 23:41:40 +0530

    initial commit

---
+++ AngularCRUD/AngularCRUD/AngularCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,115 @@
+﻿using AngularCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace AngularCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>  
+        ///   
+        /// Get All Employee  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllEmployee()
+        {
+            using (TestDataSourceEntities Obj = new TestDataSourceEntities())
+            {
+                List<Employee> Emp = Obj.Employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Employee With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (TestDataSourceEntities Obj = new TestDataSourceEntities())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.Employees.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Employee  
+        /// </summary>  
+        /// <param name="Employe"></param>  
+        /// <returns></returns>  
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (TestDataSourceEntities Obj = new TestDataSourceEntities())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Delete Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (TestDataSourceEntities Obj = new TestDataSourceEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Update Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (TestDataSourceEntities Obj = new TestDataSourceEntities())
+                {
+                    //var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employees.Where(x => x.Emp_Id == Emp.Emp_Id).FirstOrDefault();
+                    EmpObj.Emp_Age = Emp.Emp_Age;
+                    EmpObj.Emp_City = Emp.Emp_City;
+                    EmpObj.Emp_Name = Emp.Emp_Name;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ AngularCRUD/AngularCRUD/AngularCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularCRUD.Startup))]
+namespace AngularCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
